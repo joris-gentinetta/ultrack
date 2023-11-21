e05ebbfc6b4fd303e68701d3f86d9475c8729e66
jg_mac <gentinetta.joris@gmail.com>
2023-11-21 10:14:19 +0100

    renamed environments folder for euler

--- examples/stardist_2d/create_envs_euler/display.sh
+++ examples/stardist_2d/create_envs_euler/display.sh
@@ -3,3 +3,4 @@ conda env remove -p /cluster/scratch/jorisg/conda/display -y
 conda create --prefix /cluster/scratch/jorisg/conda/ python=3.10 -y
 conda activate -p /cluster/scratch/jorisg/conda/ display || exit
 conda install -c conda-forge napari pyqt
+conda install matplotlib
